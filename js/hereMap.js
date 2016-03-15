554e23608931f74fda5a00cbb9b78cf86cfd8961
afrancisboeuf <afrancisboeuf@gmail.com>
2016-03-14 21:43:07 -0700

    corrected API credentials

--- js/hereMap.js
+++ js/hereMap.js
@@ -1,7 +1,7 @@
 
 var platform = new H.service.Platform({
   'app_id': 'wqVvmXUfmkEubBVWaEJC',
-  'app_code': 'bGsmBkttNfi04TRc1N3s'
+  'app_code': 'bGsmBkttNfi04TRc1N3s-w'
 });
 
 // default map layers:
@@ -41,30 +41,33 @@ document.addEventListener("DOMContentLoaded", function(){
   function onContextMenuRequested(e) {
     
     // add contect item:
-    e.items.push(new H.util.ContextItem({
-      label: "Hello Holberton School!",
-    }));
+    // e.items.push(new H.util.ContextItem({
+    //   label: "Hello Holberton School!",
+    // }));
     
-    //define result function
-    function reverseGeocodeResult (result, contextMenuEvent) {
-        // Removes context item and add new one
-        contextMenuEvent.items.pop();
-        var label = result.Response.View[0].Result[0].Location.Address.Label;
-        contextMenuEvent.items.push(new H.util.ContextItem({
-          label: label,
-        }));
-        // Fires the context menu event again to display the new menu
-        contextMenuEvent.currentTarget.dispatchEvent(contextMenuEvent);
-        contextMenuEvent.stopPropagation();
-    };
-
-    //  call API
     var geocoder = platform.getGeocodingService();
     var parameters = {
         prox: map.screenToGeo(e.viewportX, e.viewportY).lat + ', ' + map.screenToGeo(e.viewportX, e.viewportY).lng,
         mode: 'retrieveAddresses',
         maxResults: 1
     };
+
+    //define result function
+    function reverseGeocodeResult (result, contextMenuEvent) {
+        // Removes context item and add new one
+        
+        var address = result.Response.View[0].Result[0].Location.Address.Label;
+        contextMenuEvent.items.pop();
+
+        contextMenuEvent.items.push(new H.util.ContextItem({
+          label: address,
+        }));
+
+        // Fires the context menu event again to display the new menu
+        contextMenuEvent.currentTarget.dispatchEvent(contextMenuEvent);
+        contextMenuEvent.stopPropagation();
+    };
+    
     geocoder.reverseGeocode(parameters, function(result) {
       reverseGeocodeResult(result, e);
     }, function(error) {
